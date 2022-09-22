cf45b85f6d412530ebc61480de78fc720365fcc7
Tomas Alves de Souza <tomas.alves08@gmail.com>
2022-09-22 14:58:29 +1200

    Setup general Auth0 functionalities

--- client/apis/fruits.js
+++ client/apis/fruits.js
@@ -3,7 +3,7 @@ import request from 'superagent'
 const rootUrl = '/api/v1'
 
 export function getFruits() {
-  return request.get(rootUrl + '/fruits').then((res) => {
+  return request.get(rootUrl + '/recipes').then((res) => {
     return res.body.fruits
   })
 }
--- client/index.js
+++ client/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Auth0Provider } from '@auth0/auth0-react'
 import { Provider } from 'react-redux'
 import store from './store'
 
@@ -7,9 +8,16 @@ import App from './components/App'
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    <Provider store={store}>
-      <App />
-    </Provider>,
+    <Auth0Provider
+      domain="roa22-tomas.au.auth0.com"
+      clientId="6MhzntaEMTJ3mltZq6YMNqWbsHV5okaw"
+      redirectUri={window.location.origin}
+      audience="https://seasoned/api"
+    >
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </Auth0Provider>,
     document.getElementById('app')
   )
 })
