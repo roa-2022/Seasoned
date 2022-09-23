74e1827e03ddebd0dcbf8b4243a74e96d7c8dacc
Brendan Jarvis <jarvisbrendanj@gmail.com>
2022-09-23 14:27:30 +1200

    Remove lines from produce route

--- server/routes/produce.js
+++ server/routes/produce.js
@@ -13,16 +13,4 @@ router.get('/', async (req, res) => {
   }
 })
 
-router.post('/', async (req, res) => {
-  const produceData = req.body
-  
-  {
-		"id": 2,
-		"name": "apricot",
-		"display_name": "Apricot/Aperekoti",
-		"type": "fruit",
-		"image": "https://www.5aday.co.nz/media/14967/apricot.jpg?&width=800&height=400&anchor=top&mode=crop"
-	}
-})
-
 module.exports = router
