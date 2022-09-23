7c1f46538513111ac6d4e976c5fd51329faa064f
Zin Hoang <103907978+ZinHoang@users.noreply.github.com>
2022-09-23 15:36:17 +1200

    complete routing for each recipe

--- client/components/Recipe.jsx
+++ client/components/Recipe.jsx
@@ -2,28 +2,36 @@ import React from 'react'
 import { useParams } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 
-// TODO: access using useParams
-
 export default function Recipe() {
-  // const { label, image, url } = props.recipe.recipe
-  // The second .recipe is the key within each Recipe Object
-
   const { id } = useParams()
   const recipes = useSelector((state) => state.recipes)
   const recipe = recipes[id]
-  console.log('Specific recipe: ', recipe)
+
   const label = recipe.recipe.label
   const largeImage = recipe.recipe.images.LARGE?.url
   const regularImage = recipe.recipe.images.REGULAR.url
+  const ingredientsArr = recipe.recipe.ingredients
+
+  const dietaryArr = recipe.recipe.healthLabels
+  const selectiveDietary = dietaryArr.filter((word) => {
+    return word === 'Vegan' || word === 'Vegetarian' || word === 'Gluten-Free'
+  })
 
   return (
     <div>
-      <h3>{/* <a href={url}>{label}</a> */}</h3>
-      {/* <img src={image} alt="food" /> */}
-
       <h3>{label ? label : 'Hello World'}</h3>
       <img src={largeImage ? largeImage : regularImage} alt="recipe" />
-      {/* <img src={regularImage} alt="recipe" /> */}
+      <p>
+        <strong>{selectiveDietary.map((e) => e + ' ')} </strong>
+      </p>
+      <div>
+        <h4>Ingredients</h4>
+        <ul>
+          {ingredientsArr.map((ingredient, idx) => (
+            <li key={idx}>{ingredient.text}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   )
 }
