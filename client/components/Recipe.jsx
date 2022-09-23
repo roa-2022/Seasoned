545627d03804909028f0f3564e6428442e3fbccf
Pei Zhang <peip7@outlook.com>
2022-09-24 11:05:24 +1200

    refactor the Recipe component

--- client/components/Recipe.jsx
+++ client/components/Recipe.jsx
@@ -7,32 +7,31 @@ export default function Recipe() {
   const recipes = useSelector((state) => state.recipes)
   const recipe = recipes[id]
 
-  const label = recipe.recipe.label
-  const largeImage = recipe.recipe.images.LARGE?.url
-  const regularImage = recipe.recipe.images.REGULAR.url
-  const ingredientsArr = recipe.recipe.ingredients
+  const { label, images, ingredients, healthLabels, url } = recipe.recipe
 
-  const dietaryArr = recipe.recipe.healthLabels
-  const selectiveDietary = dietaryArr.filter((word) => {
+  const largeImage = images.LARGE?.url
+  const regularImage = images.REGULAR.url
+
+  const dietary = healthLabels.filter((word) => {
     return word === 'Vegan' || word === 'Vegetarian' || word === 'Gluten-Free'
   })
 
   return (
     <div>
-      <h3>{label ? label : 'Hello World'}</h3>
-      <img src={largeImage ? largeImage : regularImage} alt="recipe" />
+      <h3>{label}</h3>
+      <img src={largeImage || regularImage} alt="recipe" />
       <p>
-        <strong>{selectiveDietary.map((e) => e + ' ')} </strong>
+        <strong>{dietary.map((e) => e + ' ')} </strong>
       </p>
       <div>
         <h4>Ingredients</h4>
         <ul>
-          {ingredientsArr.map((ingredient, idx) => (
+          {ingredients.map((ingredient, idx) => (
             <li key={idx}>{ingredient.text}</li>
           ))}
         </ul>
         <h4>Instructions</h4>
-        <a href={recipe.recipe.url}>Click this link</a>
+        <a href={url}>Click this link</a>
       </div>
     </div>
   )
