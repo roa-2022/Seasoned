fd7bee727ff515b00af36b0b079ffb9569a5a540
Brendan Jarvis <jarvisbrendanj@gmail.com>
2022-09-24 10:04:01 +1200

    Add try/catch to apis/recipes.js request

--- client/apis/recipes.js
+++ client/apis/recipes.js
@@ -5,17 +5,14 @@ import request from 'superagent'
 // TODO: create .env file to store my KEY
 // Adjust config/setup -- Dotenv
 
-export function getRecipes(userInput) {
-  return request
-    .get(
-      // `https://api.edamam.com/api/recipes/v2?type=public&q=${userInput}&KEY`
-
+export async function getRecipes(userInput) {
+  try {
+    const res = await request.get(
       `https://api.edamam.com/api/recipes/v2?type=public&q=${userInput}&app_id=eeb6d9e0&app_key=9bbd15d307d13bf463f6a7326338bced&nutrients%5BCA%5D=50%2B`
     )
-    .then((res) => {
-      // console.log(res.body)
-      const result = res.body.hits
-      console.log(result)
-      return result
-    })
+    const result = res.body.hits
+    return result
+  } catch (err) {
+    console.log('Error getting recipes from Edamam API. Error: ' + err)
+  }
 }
