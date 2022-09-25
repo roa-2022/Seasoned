bed45a3b2fb3bb061f3c76cf44e9180af89480c8
Ash Rockett <ashrockett19@gmail.com>
2022-09-26 10:14:22 +1300

    Updated theme

--- client/styles/theme.js
+++ client/styles/theme.js
@@ -1,50 +1,49 @@
 import { createTheme } from '@mui/material/styles'
 
-export const theme = createTheme({
-  typography: {
-    h1: {
-      fontFamily: "Bree, serif"
-    },
-    h2: {
-      fontFamily: "Bree, serif"
-    },
-    h3: {
-      fontFamily: "Bree, serif"
-    },
-    h4: {
-      fontFamily: "Bree, serif"
-    },
-    h5: {
-      fontFamily: "Bree, serif"
-    },
-    h6: {
-      fontFamily: "Bree, serif"
-    },
-    button: {
-      fontFamily: "Bree, serif;"
-    },
-    subtitle1: {
-      fontFamily: "Nobile, sans-serif"
-    },
-    subtitle2: {
-      fontFamily: "Nobile, sans-serif"
-    },
-    body1: {
-      fontFamily: "Nobile, sans-serif"
-    },
-    body2: {
-      fontFamily: "Nobile, sans-serif"
-    },
-  },
-  
+export const theme = createTheme ({
   palette: {
+    type: 'light',
     primary: {
-      // Purple and green play nicely together.
-      main: '#BF572B',
+      main: '#c56b02',
     },
     secondary: {
-      // This is green.A700 as hex.
-      main: '#EFF6EE',
+      main: '#3f6c51',
     },
+    info: {
+      main: '#598e17',
+    },
+  },
+  typography: {
+    fontSize: 16,
+    h1: {
+      fontFamily: '"Bree Serif", "Helvetica", "Arial", sans-serif',
+    },
+    h2: {
+      fontFamily: '"Bree Serif", "Helvetica", "Arial", sans-serif',
+    },
+    fontFamily: '"Nobile", "Helvetica", "Arial", sans-serif',
+    h3: {
+      fontFamily: '"Bree Serif", "Helvetica", "Arial", sans-serif',
+    },
+    h4: {
+      fontFamily: '"Bree Serif", "Helvetica", "Arial", sans-serif',
+    },
+    h5: {
+      fontFamily: '"Bree Serif", "Helvetica", "Arial", sans-serif',
+    },
+    h6: {
+      fontFamily: '"Bree Serif", "Helvetica", "Arial", sans-serif',
+    },
+    body1: {
+      fontWeight: 400,
+    },
+  },
+  props: {
+    MuiAppBar: {
+      color: 'secondary',
+    },
+  },
+  shape: {
+    borderRadius: 4,
   },
 });
