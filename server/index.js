9aee63de9d6d0e47bea91b5949e509d031835550
Tomas Alves de Souza <tomas.alves08@gmail.com>
2022-09-26 10:45:09 +1300

    Fix server file

--- server/index.js
+++ server/index.js
@@ -1,11 +1,9 @@
-const server = require('./server')
-
 if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
   const envConfig = require('dotenv').config()
   if (envConfig.error) throw envConfig.error
 }
 
-import server from './server'
+const server = require('./server')
 const path = require('path')
 const dotenv = require('dotenv')
 
