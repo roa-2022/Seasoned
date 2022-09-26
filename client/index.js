e6e67561245824cfbb578197c9f94b64b2895057
Brendan Jarvis <jarvisbrendanj@gmail.com>
2022-09-26 17:13:03 +1300

    Working on env

--- client/index.js
+++ client/index.js
@@ -8,12 +8,18 @@ import store from './store'
 import App from './components/App'
 
 document.addEventListener('DOMContentLoaded', () => {
+  console.log(
+    process.env
+    // process.env.AUTH0_DOMAIN,
+    // process.env.AUTH0_CLIENT_ID,
+    // process.env.AUTH0_AUDIENCE
+  )
   ReactDOM.render(
     <Auth0Provider
-      domain="roa22-tomas.au.auth0.com"
-      clientId="6MhzntaEMTJ3mltZq6YMNqWbsHV5okaw"
+      domain={process.env.AUTH0_DOMAIN}
+      clientId={process.env.AUTH0_CLIENT_ID}
       redirectUri={window.location.origin}
-      audience="https://seasoned/api"
+      audience={process.env.AUTH0_AUDIENCE}
     >
       <Provider store={store}>
         <Router>
