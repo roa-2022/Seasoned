ba61924d6706649668d0668959ab73bb2e71f9cb
Zin Hoang <103907978+ZinHoang@users.noreply.github.com>
2022-09-27 02:16:54 +1300

    add pagination to Seasonal Product

--- client/components/SeasonalProduct.jsx
+++ client/components/SeasonalProduct.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useSelector } from 'react-redux'
 import { Box, Grid, Container } from '@mui/material'
 import Typography from '@mui/material/Typography'
 import ProductCard from './ProductCard'
+import AppPagination from './AppPagination'
 
 export default function SeasonalProduct() {
-  const products = useSelector((state) => state.seasonalProduct)
+  const seasonalProducts = useSelector((state) => state.seasonalProduct)
+  const [products, setProducts] = useState([])
 
   return (
     <div>
-      {products.length > 0 && (
+      {seasonalProducts.length > 0 && (
         <Box display="flex" justifyContent={'center'} sx={{ p: 4 }}>
           <Typography variant="h5">Seasonal Products</Typography>
         </Box>
@@ -38,6 +40,9 @@ export default function SeasonalProduct() {
             </Grid>
           ))}
         </Grid>
+        {/* {products.length > 0 && ( */}
+        <AppPagination setProducts={(product) => setProducts(product)} />
+        {/* )} */}
       </Container>
     </div>
   )
