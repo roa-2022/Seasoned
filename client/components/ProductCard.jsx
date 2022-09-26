bff513e555d35e8ba37763df6d27d474c2b43906
Zin Hoang <103907978+ZinHoang@users.noreply.github.com>
2022-09-27 10:35:11 +1300

    make seasonal products card clickable

--- client/components/ProductCard.jsx
+++ client/components/ProductCard.jsx
@@ -4,7 +4,7 @@ import Card from '@mui/material/Card'
 import CardContent from '@mui/material/CardContent'
 import CardMedia from '@mui/material/CardMedia'
 import Typography from '@mui/material/Typography'
-import { Button, CardActionArea, CardActions } from '@mui/material'
+import { CardActionArea } from '@mui/material'
 
 import { fetchRecipes } from '../actions'
 
@@ -13,32 +13,31 @@ export default function ProductCard({ product }) {
 
   return (
     <Card sx={{ maxWidth: 345 }}>
-      <CardActionArea>
+      <CardActionArea
+        onClick={(e) => {
+          e.preventDefault()
+          dispatch(fetchRecipes(product.name))
+        }}
+      >
         <CardMedia
           component="img"
           height="140"
           image={product.image_url}
           alt={product.name}
         />
+
         <CardContent>
-          <Typography gutterBottom variant="h6" component="div">
+          <Typography
+            gutterBottom
+            variant="h6"
+            component="div"
+            sx={{ fontSize: '16px' }}
+          >
             {/* Converting the first letter to UpperCase using Regex */}
             {product.name.replace(/^./, product.name[0].toUpperCase())}
           </Typography>
         </CardContent>
       </CardActionArea>
-      <CardActions>
-        <form
-          onSubmit={(e) => {
-            e.preventDefault()
-            dispatch(fetchRecipes(product.name))
-          }}
-        >
-          <Button size="small" color="primary" type="submit">
-            Search Recipe
-          </Button>
-        </form>
-      </CardActions>
     </Card>
   )
 }
