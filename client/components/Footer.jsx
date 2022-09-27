ed3a9d8ab3ff6e9ecf7755eda0a790c3c26d3428
Brendan Jarvis <jarvisbrendanj@gmail.com>
2022-09-27 22:34:50 +1300

    Fix spacing error

--- client/components/Footer.jsx
+++ client/components/Footer.jsx
@@ -4,26 +4,28 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCarrot } from '@fortawesome/free-solid-svg-icons'
 
 export default function Footer() {
-
   return (
-    <Box 
+    <Box
       sx={{
-        backgroundColor: "#3F6C51",
-        color: "white",
-        bottom: "-1px",
-        width: "100%",
-      }}>
-      <Container maxWidth="lg" >
-        <Grid container spacing paddingBottom={10} paddingTop={2}>
+        backgroundColor: '#3F6C51',
+        color: 'white',
+        bottom: '-1px',
+        width: '100%',
+      }}
+    >
+      <Container maxWidth="lg">
+        <Grid container paddingBottom={10} paddingTop={2}>
           <Grid item xs={12}>
-              <Box paddingBottom={1} color="inherit">
-                <Link underline="none" href="/" color="inherit">
-                <Typography variant="h6">Seasoned <FontAwesomeIcon icon={faCarrot} /></Typography>
-                </Link>
-              </Box>
-            <Box sx={{ fontSize: "0.8em" }}>
+            <Box paddingBottom={1} color="inherit">
+              <Link underline="none" href="/" color="inherit">
+                <Typography variant="h6">
+                  Seasoned <FontAwesomeIcon icon={faCarrot} />
+                </Typography>
+              </Link>
+            </Box>
+            <Box sx={{ fontSize: '0.8em' }}>
               <Box paddingBottom={1} paddingTop={1} color="inherit">
-                <Link  underline="none" href="/" color="inherit">
+                <Link underline="none" href="/" color="inherit">
                   Contact Us
                 </Link>
               </Box>
