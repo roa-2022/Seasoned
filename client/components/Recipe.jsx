da3133eba94d310587a9cd4c609de1c380b35d0b
Brendan Jarvis <jarvisbrendanj@gmail.com>
2022-09-28 10:14:39 +1300

    Fix button

--- client/components/Recipe.jsx
+++ client/components/Recipe.jsx
@@ -8,7 +8,7 @@ import FavoriteBorder from '@mui/icons-material/FavoriteBorder'
 import Favorite from '@mui/icons-material/Favorite'
 import { pink } from '@mui/material/colors'
 import { OpenInNew } from '@mui/icons-material'
-import { Box, List, ListItem, Typography, Button } from '@mui/material'
+import { Box, List, Link, ListItem, Typography, Button } from '@mui/material'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSeedling, faBook } from '@fortawesome/free-solid-svg-icons'
 import { postFavouriteProduct } from '../apis/produce'
@@ -51,86 +51,98 @@ export default function Recipe() {
       )}
       {label && (
         <>
-          <Box sx={{
-            margin: "0px auto",
-            width: {
-              xs: "90%", // theme.breakpoints.up('xs')
-              sm: 200, // theme.breakpoints.up('sm')
-              md: 300, // theme.breakpoints.up('md')
-              lg: 400, // theme.breakpoints.up('lg')
-              xl: 500, // theme.breakpoints.up('xl')
-            },
-          }}>
-          <Typography sx={{
-            textAlign: "center",
-            marginBottom: "30px",
-          }} 
-          variant="h4">{label}</Typography>
-            <img width={"100%"} src={image} alt={label} />
-          
-          <FormGroup>
-            <FormControlLabel
-              control={
-                <Checkbox
-                  checked={checked}
-                  onChange={handleFavourite}
-                  icon={<FavoriteBorder />}
-                  checkedIcon={<Favorite />}
-                  sx={{
-                    color: pink[800],
-                    '&.Mui-checked': {
-                      color: pink[600],
-                    },
-                  }}
-                />
-              }
-              label="Add to Favourite"
-            />
-          </FormGroup>
+          <Box
+            sx={{
+              margin: '0px auto',
+              width: {
+                xs: '90%', // theme.breakpoints.up('xs')
+                sm: 200, // theme.breakpoints.up('sm')
+                md: 300, // theme.breakpoints.up('md')
+                lg: 400, // theme.breakpoints.up('lg')
+                xl: 500, // theme.breakpoints.up('xl')
+              },
+            }}
+          >
+            <Typography
+              sx={{
+                textAlign: 'center',
+                marginBottom: '30px',
+              }}
+              variant="h4"
+            >
+              {label}
+            </Typography>
+            <img width={'100%'} src={image} alt={label} />
+
+            <FormGroup>
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={checked}
+                    onChange={handleFavourite}
+                    icon={<FavoriteBorder />}
+                    checkedIcon={<Favorite />}
+                    sx={{
+                      color: pink[800],
+                      '&.Mui-checked': {
+                        color: pink[600],
+                      },
+                    }}
+                  />
+                }
+                label="Add to Favourite"
+              />
+            </FormGroup>
           </Box>
         </>
       )}
-        <Box sx={{
-          margin: "0px auto",
+      <Box
+        sx={{
+          margin: '0px auto',
           width: {
-            xs: "90%", // theme.breakpoints.up('xs')
+            xs: '90%', // theme.breakpoints.up('xs')
             sm: 200, // theme.breakpoints.up('sm')
             md: 300, // theme.breakpoints.up('md')
             lg: 400, // theme.breakpoints.up('lg')
             xl: 500, // theme.breakpoints.up('xl')
           },
-        }}>
+        }}
+      >
         <Typography>
           <strong>{dietary?.map((e) => e + ' ')} </strong>
         </Typography>
         <div>
           {ingredients && (
             <>
-              <Typography paddingTop={"20px"} variant="h4">Ingredients{' '}<FontAwesomeIcon color="#3F6C51" icon={faSeedling} /></Typography> 
+              <Typography paddingTop={'20px'} variant="h4">
+                Ingredients{' '}
+                <FontAwesomeIcon color="#3F6C51" icon={faSeedling} />
+              </Typography>
               <List>
                 {ingredients.map((ingredient, idx) => (
                   <ListItem key={idx}>{ingredient.text}</ListItem>
                 ))}
               </List>
 
-              <Typography paddingTop={"20px"} variant="h4">Instructions{' '}<FontAwesomeIcon color="#BF572B" icon={faBook} /></Typography>
+              <Typography paddingTop={'20px'} variant="h4">
+                Instructions <FontAwesomeIcon color="#BF572B" icon={faBook} />
+              </Typography>
               <Box>
-                <Button sx={{
-                  textAlign: "center",
-                }} 
-                component="a" 
-                href={url} 
-                variant="body1" 
-                startDecorator={<OpenInNew />} 
-                color="#3F6C51">
-                  Recipe Here
+                <Button
+                  variant="body1"
+                  startIcon={<OpenInNew />}
+                  target="_blank"
+                  href={url}
+                  rel="noreferrer"
+                  color="#3F6C51"
+                  aria-label="Open recipe in new tab"
+                >
+                  Recipe here!
                 </Button>
               </Box>
-              
             </>
           )}
         </div>
-
       </Box>
     </div>
   )
