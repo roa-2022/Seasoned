b87d02720af9662710166d1a649a5cec85eee322
Brendan Jarvis <jarvisbrendanj@gmail.com>
2022-09-28 22:36:07 +1300

    Add no recieps found error

--- client/components/Recipes.jsx
+++ client/components/Recipes.jsx
@@ -17,119 +17,79 @@ import {
 
 export default function Recipes() {
   const recipes = useSelector((state) => state.recipes)
-  const dietary = useSelector((state) => state.dietary)
   const ingredient = useSelector((state) => state.ingredient)
-
-  const shortlistedRecipe = recipes.filter((recipe) => {
-    const filtered = dietary.every((dietaryType) =>
-      recipe.recipe.healthLabels.includes(dietaryType)
-    )
-    if (filtered) {
-      return recipe
-    }
-  })
 
   return (
     <div>
       <CssBaseline />
-      <Container sx={{ py: 2 }} maxWidth="md">
+      <Container sx={{ py: 0.5 }} maxWidth="md">
         <Grid container spacing={4}>
-          {shortlistedRecipe
-            ? shortlistedRecipe.map((recipe, idx) => {
-                const { image, label, cuisineType, dietLabels, mealType } =
-                  recipe.recipe
-                return (
-                  <Grid item key={idx} xs={12} sm={6} md={4}>
-                    <Card
-                      sx={{
-                        height: '100%',
-                        display: 'flex',
-                        flexDirection: 'column',
-                      }}
-                    >
-                      <CardActionArea
+          {recipes.length >= 1 ? (
+            recipes.map((recipe, idx) => {
+              const { image, label, cuisineType, dietLabels, mealType } =
+                recipe.recipe
+              return (
+                <Grid item key={idx} xs={12} sm={6} md={4}>
+                  <Card
+                    sx={{
+                      height: '100%',
+                      display: 'flex',
+                      flexDirection: 'column',
+                    }}
+                  >
+                    <CardActionArea>
+                      <CardMedia component="img" image={image} alt={label} />
+                      <CardContent sx={{ flexGrow: 1 }}>
+                        <Typography gutterBottom variant="h5" component="h2">
+                          {label}
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                          Cuisine Type:{' '}
+                          {cuisineType.map(
+                            (string) =>
+                              string[0].toUpperCase() + string.substring(1)
+                          )}
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                          Diet Labels:{' '}
+                          {dietLabels.map((text) => {
+                            const addSpace = text + ' '
+                            return addSpace
+                          })}
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                          Meal Type: {mealType}
+                        </Typography>
+                      </CardContent>
+                    </CardActionArea>
+                    <CardActions>
+                      <Button
                         component={Link}
                         to={`/recipes/${ingredient}/${idx}`}
+                        size="small"
+                        color="primary"
                       >
-                        <CardMedia component="img" image={image} alt={label} />
-                        <CardContent sx={{ flexGrow: 1 }}>
-                          <Typography gutterBottom variant="h5" component="h2">
-                            {label}
-                          </Typography>
-                          <Typography variant="body2" color="text.secondary">
-                            Cuisine Type:{' '}
-                            {cuisineType.map(
-                              (string) =>
-                                string[0].toUpperCase() + string.substring(1)
-                            )}
-                          </Typography>
-                          <Typography variant="body2" color="text.secondary">
-                            Diet Labels:{' '}
-                            {dietLabels.map((text) => {
-                              const addSpace = text + ' '
-                              return addSpace
-                            })}
-                          </Typography>
-                          <Typography variant="body2" color="text.secondary">
-                            Meal Type: {mealType}
-                          </Typography>
-                        </CardContent>
-                      </CardActionArea>
-                    </Card>
-                  </Grid>
-                )
-              })
-            : recipes.map((recipe, idx) => {
-                const { image, label, cuisineType, dietLabels, mealType } =
-                  recipe.recipe
-                return (
-                  <Grid item key={idx} xs={12} sm={6} md={4}>
-                    <Card
-                      sx={{
-                        height: '100%',
-                        display: 'flex',
-                        flexDirection: 'column',
-                      }}
-                    >
-                      <CardActionArea>
-                        <CardMedia component="img" image={image} alt={label} />
-                        <CardContent sx={{ flexGrow: 1 }}>
-                          <Typography gutterBottom variant="h5" component="h2">
-                            {label}
-                          </Typography>
-                          <Typography variant="body2" color="text.secondary">
-                            Cuisine Type:{' '}
-                            {cuisineType.map(
-                              (string) =>
-                                string[0].toUpperCase() + string.substring(1)
-                            )}
-                          </Typography>
-                          <Typography variant="body2" color="text.secondary">
-                            Diet Labels:{' '}
-                            {dietLabels.map((text) => {
-                              const addSpace = text + ' '
-                              return addSpace
-                            })}
-                          </Typography>
-                          <Typography variant="body2" color="text.secondary">
-                            Meal Type: {mealType}
-                          </Typography>
-                        </CardContent>
-                      </CardActionArea>
-                      <CardActions>
-                        <Button
-                          component={Link}
-                          to={`/recipes/${ingredient}/${idx}`}
-                          size="small"
-                          color="primary"
-                        >
-                          Recipe Here
-                        </Button>
-                      </CardActions>
-                    </Card>
-                  </Grid>
-                )
-              })}
+                        Recipe Here
+                      </Button>
+                    </CardActions>
+                  </Card>
+                </Grid>
+              )
+            })
+          ) : ingredient ? (
+            <Grid
+              container
+              justifyContent="center"
+              sx={{ position: 'absolute' }}
+            >
+              <Typography
+                variant="body2"
+                sx={{ padding: '20px', textAlign: 'center', color: 'red' }}
+              >
+                No Recipes Found!
+              </Typography>
+            </Grid>
+          ) : null}
         </Grid>
       </Container>
     </div>
